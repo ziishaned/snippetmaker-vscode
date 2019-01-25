605959196b2efe8994b08c7ee7a61fa9e1eda370
Zeeshan Ahmad <ziishaned@gmail.com>
2019-01-25 22:18:46 +0400

    Rename file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import {ExtensionContext, commands} from 'vscode';
 
-import {SnippetMaker} from './SnippetMaker';
+import {SnippetMaker} from './snippet-maker';
 
 export const activate = (context: ExtensionContext) => {
   let disposable = commands.registerCommand('snippetmaker.make_snippet', async () => {
